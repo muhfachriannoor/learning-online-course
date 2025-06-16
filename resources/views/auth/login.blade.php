c90c44f74aad729ea5f46d8806a2eba0c1c71347
muhfachriannoor <muhammadfachriannoor@gmail.com>
2025-06-16 09:41:35 +0800

    feat: UI Login and Add Component navbar-guest

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,58 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="{{ asset('output.css') }}" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <title>Sign In - Obito Online Learning Platform</title>
+        <meta name="description" content="Obito is an innovative online learning platform that empowers students and professionals with high-quality, accessible courses.">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/logos/logo-64.png') }}">
+        <link rel="apple-touch-icon" href="{{ asset('assets/images/logos/logo-64.png') }}">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        <!-- Open Graph Meta Tags -->
+        <meta property="og:title" content="Obito Online Learning Platform - Learn Anytime, Anywhere">
+        <meta property="og:description" content="Obito is an innovative online learning platform that empowers students and professionals with high-quality, accessible courses.">
+        <meta property="og:image" content="https://obito-platform.netlify.app/assets/images/logos/logo-64-big.png">
+        <meta property="og:url" content="https://obito-platform.netlify.app">
+        <meta property="og:type" content="website">
+    </head>
+    <body>
+        <x-nav-guest/>
+        <main class="relative flex flex-1 h-full">
+            <section class="flex flex-1 items-center py-5 px-5 pl-[calc(((100%-1280px)/2)+75px)]">
+                <form method="POST" action="{{ route('login') }}" class="flex flex-col h-fit w-[510px] shrink-0 rounded-[20px] border border-obito-grey p-5 gap-5 bg-white">
+                    @csrf
+                    <h1 class="font-bold text-[22px] leading-[33px] mb-5">Welcome Back, <br>Let's Upgrade Skills</h1>
+                    <div class="flex flex-col gap-2">
+                        <p>Email Address</p>
+                        <label class="relative group">
+                            <input name="email" type="email" class="appearance-none outline-none w-full rounded-full border border-obito-grey py-[14px] px-5 pl-12 font-semibold placeholder:font-normal placeholder:text-obito-text-secondary group-focus-within:border-obito-green transition-all duration-300" placeholder="Type your valid email address">
+                            <img src="{{ asset('assets/images/icons/sms.svg') }}" class="absolute size-5 flex shrink-0 transform -translate-y-1/2 top-1/2 left-5" alt="icon">
+                        </label>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+                    <div class="flex flex-col gap-3">
+                        <p>Password</p>
+                        <label class="relative group">
+                            <input name="password" type="password" class="appearance-none outline-none w-full rounded-full border border-obito-grey py-[14px] px-5 pl-12 font-semibold placeholder:font-normal placeholder:text-obito-text-secondary group-focus-within:border-obito-green transition-all duration-300" placeholder="Type your password">
+                            <img src="{{ asset('assets/images/icons/shield-security.svg') }}" class="absolute size-5 flex shrink-0 transform -translate-y-1/2 top-1/2 left-5" alt="icon">
+                        </label>
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                        <a href="#" class="text-sm text-obito-green hover:underline">Forgot My Password</a>
+                    </div>
+                    <button type="submit" class="flex items-center justify-center gap-[10px] rounded-full py-[14px] px-5 bg-obito-green hover:drop-shadow-effect transition-all duration-300">
+                        <span class="font-semibold text-white">Sign In to My Account</span>
+                    </button>
+                </form>
+            </section>
+            <div class="relative flex w-1/2 shrink-0">
+                <div id="background-banner" class="absolute flex w-full h-full overflow-hidden">
+                    <img src="{{ asset('assets/images/backgrounds/banner-subscription.png') }}" class="w-full h-full object-cover" alt="banner">
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
