5adcde7b39f5a5e7c815cbc30c3016e821cffc5b
muhfachriannoor <muhammadfachriannoor@gmail.com>
2025-06-16 20:35:41 +0800

    feat: Integration Midtrans Snap Payment & UI Checkout Success

--- resources/views/front/checkout.blade.php
+++ resources/views/front/checkout.blade.php
@@ -13,7 +13,9 @@
     </div>
     <main class="flex flex-1 justify-center py-5 items-center">
         <div class="flex w-[1000px] !h-fit rounded-[20px] border border-obito-grey gap-[40px] bg-white items-center p-5">
-            <form id="checkout-details" action="success-checkout.html" class="w-full flex flex-col gap-5">
+            <form id="checkout-details" method="POST" class="w-full flex flex-col gap-5">
+                @csrf
+                <input type="text" hidden name="payment_method" value="Midtrans">
                 <h1 class="font-bold text-[22px] leading-[33px]">Checkout Pro</h1>
                 <section id="give-access-to" class="flex flex-col gap-2">
                     <h2 class="font-semibold">Give Access to</h2>
@@ -85,7 +87,7 @@
                             <p class="font-semibold">Cancel</p>
                         </div>
                     </a>
-                    <button type="submit" class="flex text-white bg-obito-green rounded-full items-center justify-center py-[10px] hover:drop-shadow-effect transition-all duration-300">
+                    <button id="pay-button" type="submit" class="flex text-white bg-obito-green rounded-full items-center justify-center py-[10px] hover:drop-shadow-effect transition-all duration-300">
                         <p class="font-semibold">Pay Now</p>
                     </button>
                 </div>
@@ -131,4 +133,52 @@
 @endsection
 @push('after-scripts')
     <script src="{{ asset('js/dropdown-navbar.js') }}"></script>
+
+    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.clientKey') }}"></script>
+
+    <script type="text/javascript">
+        const payButton = document.getElementById('pay-button');
+        payButton.addEventListener('click', function(e) {
+            e.preventDefault();
+            // Fetch the Snap token from your backend
+            fetch('{{ route('front.payment_store_midtrans') }}', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": document.querySelector('input[name="_token"]').value
+                },
+                body: JSON.stringify({
+                    // Any additional data you want to send with the request
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if(data.snap_token) {
+                    // Trigger Midtrans Snap payment popup
+                    snap.pay(data.snap_token, {
+                        onSuccess: function(result) {
+                            window.location.href = "{{ route('front.checkout.success') }}";
+                        },
+                        onPending: function(result) {
+                            alert('Payment Pending!');
+                            window.location.href = "{{ route('front.index') }}";
+                        },
+                        onError: function(result) {
+                            alert('Payment failed: ' + result.status_message);
+                            window.location.href = "{{ route('front.index') }}";
+                        },
+                        onClose: function() {
+                            alert('Payment popup closed');
+                            window.location.href = "{{ route('front.index') }}";
+                        }
+                    });
+                }else{
+                    alert('Error: ' + data.error);
+                }
+            })
+            .catch(error => {
+                console.log('Error:', error);
+            });
+        });
+    </script>
 @endpush
